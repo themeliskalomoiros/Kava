4d742f9db2569f473f7858124f7d977e54c656df
Themelis Kalomoiros <themelis.kalomoiros@teutologic.com>
2019-07-20 17:50:00 +0200

    Removed Bottle class and Item is not abstract

--- app/src/main/java/kalymnos/skemelio/kava/Model/Item.java
+++ app/src/main/java/kalymnos/skemelio/kava/Model/Item.java
@@ -1,29 +1,34 @@
 package kalymnos.skemelio.kava.Model;
 
-public abstract class Item {
+public class Item {
     public final int categoryId;
     public final String picturePath;
     public final String title;
+    public final String volume;
 
-    public Item(int categoryId, String picturePath, String title) {
+    public Item(int categoryId, String picturePath, String title, String volume) {
         this.categoryId = categoryId;
         this.picturePath = picturePath;
         this.title = title;
+        this.volume = volume;
     }
 
     @Override
     public String toString() {
-        return title;
+        return title + ", " + volume;
     }
 
     @Override
     public boolean equals(Object obj) {
-        Item item = (Item) obj;
-        if (item == null) return false;
-        return hasSameValuesWith(item);
+        Item otherItem = (Item) obj;
+        if (otherItem == null) return false;
+        return hasSameValuesWith(otherItem);
     }
 
     protected boolean hasSameValuesWith(Item item) {
-        return categoryId == item.categoryId && picturePath == item.picturePath && title == item.title;
+        return categoryId == item.categoryId
+                && picturePath.equals(item.picturePath)
+                && title.equals(item.title)
+                && volume.equals(item.volume);
     }
 }
