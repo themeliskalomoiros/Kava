8056e5c89e2d5196fbd2c7fee47af53284159e8d
Themelis Kalomoiros <themelis.kalomoiros@teutologic.com>
2019-07-20 18:26:19 +0200

    MainScreen fetches the data from json and displays categories

--- app/src/main/java/kalymnos/skemelio/kava/Controller/MainActivity.java
+++ app/src/main/java/kalymnos/skemelio/kava/Controller/MainActivity.java
@@ -1,22 +1,37 @@
 package kalymnos.skemelio.kava.Controller;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.loader.app.LoaderManager;
+import androidx.loader.content.AsyncTaskLoader;
+import androidx.loader.content.Loader;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
-import android.widget.Toast;
 
-import java.util.ArrayList;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.List;
 
+import kalymnos.skemelio.kava.Model.Categories;
 import kalymnos.skemelio.kava.Model.Category;
-import kalymnos.skemelio.kava.R;
 import kalymnos.skemelio.kava.View.MainScreenViewMvc;
 import kalymnos.skemelio.kava.View.MainScreenViewMvcImpl;
 
-public class MainActivity extends AppCompatActivity implements MainScreenViewMvc.OnCategoryClickListener {
+public class MainActivity extends AppCompatActivity
+        implements MainScreenViewMvc.OnCategoryClickListener,
+        LoaderManager.LoaderCallbacks<List<Category>> {
+
+    private static final int LOADER_ID = 1212;
 
     private MainScreenViewMvc viewMvc;
+    private List<Category> categories;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,22 +39,70 @@ public class MainActivity extends AppCompatActivity implements MainScreenViewMvc
         viewMvc = new MainScreenViewMvcImpl(LayoutInflater.from(this), null);
         viewMvc.setOnCategoryClickListener(this);
         setContentView(viewMvc.getRootView());
-        //TODO: fetch real data
-        Category category1 = new Category(1,"ela",null);
-        Category category2 = new Category(2,"pame",null);
-        Category category3 = new Category(3,"dwse",null);
-        Category category4 = new Category(3,"rixe",null);
-        List<Category> list = new ArrayList<>();
-        list.add(category1);
-        list.add(category2);
-        list.add(category3);
-        list.add(category4);
-        viewMvc.bind(list);
+        getSupportLoaderManager().restartLoader(LOADER_ID,null,this);
     }
 
     @Override
     public void onCategoryClick(int position) {
-        //TODO: Remove toast after implementing the method
-        Toast.makeText(this, "Pressed " + (position + 1), Toast.LENGTH_SHORT).show();
+
+    }
+
+    @NonNull
+    @Override
+    public Loader<List<Category>> onCreateLoader(int id, @Nullable Bundle args) {
+        return new AsyncTaskLoader<List<Category>>(this) {
+
+            @Override
+            protected void onStartLoading() {
+                viewMvc.showProgressBar();
+                forceLoad();
+            }
+
+            @Nullable
+            @Override
+            public List<Category> loadInBackground() {
+                String rawJson = getDataFromAssets("data.json");
+                try {
+                    return Categories.from(new JSONObject(rawJson));
+                } catch (JSONException e) {
+                    return null;
+                }
+            }
+
+            private String getDataFromAssets(String fileName) {
+                BufferedReader in = null;
+                try {
+                    in = new BufferedReader(new InputStreamReader(getContext().getAssets().open(fileName)));
+                    StringBuilder sb = new StringBuilder();
+                    String line;
+                    while ((line = in.readLine()) != null) {
+                        sb.append(line);
+                    }
+                    return sb.toString();
+                } catch (IOException e) {
+                    Log.d(MainActivity.class.getName(), e.getMessage());
+                    return null;
+                } finally {
+                    if (in != null) {
+                        try {
+                            in.close();
+                        } catch (IOException e) {
+                            Log.d(MainActivity.class.getName(), e.getMessage());
+                        }
+                    }
+                }
+            }
+        };
+    }
+
+    @Override
+    public void onLoadFinished(@NonNull Loader<List<Category>> loader, List<Category> data) {
+        viewMvc.hideProgressBar();
+        viewMvc.bind(data);
+    }
+
+    @Override
+    public void onLoaderReset(@NonNull Loader<List<Category>> loader) {
+
     }
 }
