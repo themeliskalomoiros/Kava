3a4b0422a05e190e5819f38386d8bd23ef60bb16
Themelis Kalomoiros <themelis.kalomoiros@teutologic.com>
2019-07-21 06:48:07 +0200

    Category implements Parcelable

--- app/src/main/java/kalymnos/skemelio/kava/Model/Category.java
+++ app/src/main/java/kalymnos/skemelio/kava/Model/Category.java
@@ -1,8 +1,11 @@
 package kalymnos.skemelio.kava.Model;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import java.util.HashMap;
 
-public class Category {
+public class Category implements Parcelable {
 
     public final int id;
     public final String title;
@@ -12,5 +15,35 @@ public class Category {
         this.id = id;
         this.title = title;
         this.items = items;
+    }
+
+    protected Category(Parcel in) {
+        id = in.readInt();
+        title = in.readString();
+        items = new HashMap<>();
+        in.readHashMap(getClass().getClassLoader());
+    }
+
+    public static final Creator<Category> CREATOR = new Creator<Category>() {
+        @Override
+        public Category createFromParcel(Parcel in) {
+            return new Category(in);
+        }
+
+        @Override
+        public Category[] newArray(int size) {
+            return new Category[size];
+        }
+    };
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeInt(id);
+        parcel.writeString(title);
     }
 }
